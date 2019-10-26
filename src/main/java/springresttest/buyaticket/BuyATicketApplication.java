9c0dbc007b4662399fcae86ee10aa62bca6c9238
WiktorWos <wiktor.wos99@gmail.com>
2019-10-26 13:28:19 +0200

    initial commit

---
+++ src/main/java/springresttest/buyaticket/BuyATicketApplication.java
@@ -0,0 +1,13 @@
+package springresttest.buyaticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyATicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyATicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springresttest/buyaticket/BuyATicketApplicationTests.java
@@ -0,0 +1,13 @@
+package springresttest.buyaticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyATicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

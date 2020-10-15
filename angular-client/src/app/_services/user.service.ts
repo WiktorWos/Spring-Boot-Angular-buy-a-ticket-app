92b7a6abc5b9691d178766d74a5516b8ba308d6d
WiktorWos <wiktor.wos99@gmail.com>
2020-10-15 21:57:50 +0200

    services and interceptor implementation

--- angular-client/src/app/_services/user.service.ts
+++ angular-client/src/app/_services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+const API_URL = 'http://localhost:8080/api/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  geUsers(): Observable<any> {
+    return this.http.get(API_URL, { responseType: 'text' });
+  }
 }
